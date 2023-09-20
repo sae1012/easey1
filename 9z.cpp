bf79710310a64d53650df2922a31ec73eae69ab2
grigory karpunin <143996987+sae1012@users.noreply.github.com>
2023-09-20 17:00:49 +0300

    Update 9z.cpp

--- 9z.cpp
+++ 9z.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "easy.h"
 using namespace std;
-double itc_fmax(double num, double num2){
-    if (num < num2){
+double itc_fmax(double num1, double num2){
+    if (num1 < num2){
         return num2;
     }
 }
