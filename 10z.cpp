57b2ef45a8f384d3107aff52b5dc1626dc145b0d
grigory karpunin <143996987+sae1012@users.noreply.github.com>
2023-09-20 17:08:47 +0300

    Update 10z.cpp

--- 10z.cpp
+++ 10z.cpp
@@ -2,7 +2,10 @@
 #include "easy.h"
 using namespace std;
 double itc_fmin(double num, double num2){
-    if (num < num2){
+    if (num <= num2){
         return num;
+    }
+    else{
+        return num2;
     }
 }
