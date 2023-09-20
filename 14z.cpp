5d7c23ac9aa16c4f4a0f55229636dc748e7a358b
grigory karpunin <143996987+sae1012@users.noreply.github.com>
2023-09-20 17:11:09 +0300

    Update 14z.cpp

--- 14z.cpp
+++ 14z.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <math.h>
 #include "easy.h"
 using namespace std;
 int itc_str(int a, int b, int c){
-    int p, S;
+    double p;
+    int S;
     if (a + b > c && a + c > b && b + c > a ){
         p = (a + b + c) / 2;
         S = itc_sqrt((int)p * (p - a) * (p - b) * (p - c));
@@ -13,4 +13,3 @@ int itc_str(int a, int b, int c){
         return -1;
     }
 }
-
